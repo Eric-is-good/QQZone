b8c85d4a504a455943d6fad50424af86cda28295
Eric <eric123456123456@163.com>
2022-02-05 15:36:57 +0800

    jdbc 配置提取

--- src/main/java/myssm/basedao/ConnUtil.java
+++ src/main/java/myssm/basedao/ConnUtil.java
@@ -1,27 +1,34 @@
 package myssm.basedao;
 
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnUtil {
 
     private static ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
-    //private static ThreadLocal<Object> threadLocal2 = new ThreadLocal<>();
-    //private static ThreadLocal<Object> threadLocal3 = new ThreadLocal<>();
 
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver" ;
-    public static final String URL = "jdbc:mysql://localhost:3306/qqzone?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8";
-    public static final String USER = "root";
-    public static final String PWD = "123456" ;
+    static Properties properties = new Properties();
+
+    static{
+        InputStream is = ConnUtil.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static Connection createConn(){
         try {
-            //1.加载驱动
-            Class.forName(DRIVER);
-            //2.通过驱动管理器获取连接对象
-            return DriverManager.getConnection(URL, USER, PWD);
-        } catch (ClassNotFoundException | SQLException e) {
+            DataSource druidDataSource = DruidDataSourceFactory.createDataSource(properties);
+            return druidDataSource.getConnection();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null ;
